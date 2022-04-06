f2867269c4d737267d2358753e1f06cfdc599c4e
Andrey Girba <andrey.girba@gmail.com>
2022-04-06 21:23:20 +0300

    @Qualifier ftw

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,11 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
+    private Long nanoTime = System.currentTimeMillis();
 
     public Long getTime() {
         return nanoTime;
