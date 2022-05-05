84352ea6dd7331588b2e0e2581e604ba27048a84
Andrey Girba <andrey.girba@gmail.com>
2022-05-05 20:47:34 +0300

    after review

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,27 +7,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
-    private Animal animal1;
-    private Animal animal2;
-    private Timer time;
+    private Timer timer = new Timer();
 
     @Autowired
-    public AnimalsCage(@Qualifier("cat") Animal animal1, @Qualifier("dog") Animal animal2, Timer time) {
-        this.animal1 = animal1;
-        this.animal2 = animal2;
-        this.time = time;
-    }
+    @Qualifier("dog")
+    private Animal animal;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal1.toString() + "," + animal2.toString());
+        System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(time.getTime());
+        System.out.println(this.timer.getTime());
         System.out.println("________________________");
     }
 
     public Timer getTimer() {
-        Timer timer = new Timer();
         return timer;
     }
 }
